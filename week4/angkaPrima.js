fed1dcbf7823849c96ae1e236a858c9dfe712691
GustiPutraJarmana <gustimertaputra@gmail.com>
2019-12-21 16:48:26 +0800

    week 6

--- week4/angkaPrima.js
+++ week4/angkaPrima.js
@@ -14,3 +14,25 @@ console.log(angkaPrima(7)); // true
 console.log(angkaPrima(6)); // false
 console.log(angkaPrima(23)); // true
 console.log(angkaPrima(33)); // false
+function rangePrimeNumber(start, finish) {
+  let i = 2;
+  let result = [];
+  while (result.length <= finish) {
+    if (i === 2 || i === 3 || i === 5 || i === 7) {
+      result.push(i);
+    } else {
+      if (i % 2 !== 0 && i % 3 !== 0 && i % 5 !== 0 && i % 7 !== 0) {
+        result.push(i);
+      }
+    }
+    i++;
+  }
+  let output = [];
+  for (let i = start - 1; i < result.length - 1; i++) {
+    output.push(result[i]);
+  }
+  return output;
+}
+console.log(rangePrimeNumber(5, 10));
+// output
+// [11,13,17,19,23,29]
